79b8679bb0693e59b6a91b0ebe59b509be25ca42
noemy365 <noemyfelix4@gmail.com>
2026-04-07 15:09:09 -0300

    feat: adicionei ex1

---
+++ ex01.js
@@ -0,0 +1,18 @@
+// const readline = require('readline-sync')
+
+// let nome = readline.question("Digite seu nome: ")
+
+// console.log (`Seu nome é: ${nome}`)
+
+//-----------------
+
+const readline = require('readline-sync')
+
+console.log('========= SISTEMA DE CADASTRO ==========')
+console.log(' ')
+let nome = readline.question(`digite seu nome:`)
+let curso = readline.question('curso que está matriculado:')
+let idade = readline.questionInt('informe a sua idade:')
+
+//-------------------
+
